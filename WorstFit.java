aeaac624286d9534e4b27533f036c17c6301863a
Blueturboguy07 <114044325+Blueturboguy07@users.noreply.github.com>
2024-02-12 01:11:31 -0600

    WorstFit class

---
+++ WorstFit.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class WorstFit {
+
+
+    public static void main(String args[]) throws FileNotFoundException{
+
+        Scanner input = new Scanner(new File("input20.txt"));
+
+        input.close();
+
+    }
+    
+
+
+
+}
