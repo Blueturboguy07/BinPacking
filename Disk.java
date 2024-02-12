ed592e5740787f3fd2fd3d19ad3cc7086f6531c6
Blueturboguy07 <114044325+Blueturboguy07@users.noreply.github.com>
2024-02-11 19:09:04 -0600

    format document

--- Disk.java
+++ Disk.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Disk implements Comparable<Disk> {
-    private static final int DISK_CAPACITY = 1024; // 1GB in MB
+    private static final int DISK_CAPACITY = 1024;
     private List<File> files;
-    
+
     public Disk() {
         this.files = new ArrayList<>();
     }
-    
+
     public int getRemainingSpace() {
         int usedSpace = 0;
         for (File file : files) {
@@ -16,7 +16,7 @@ public class Disk implements Comparable<Disk> {
         }
         return DISK_CAPACITY - usedSpace;
     }
-    
+
     public void addFile(File file) {
         if (getRemainingSpace() >= file.getSize()) {
             files.add(file);
@@ -24,27 +24,25 @@ public class Disk implements Comparable<Disk> {
             System.out.println("Not enough space to add file: " + file.getName());
         }
     }
-    
+
     @Override
     public int compareTo(Disk otherDisk) {
-        // Compare disks based on remaining space
         return Integer.compare(this.getRemainingSpace(), otherDisk.getRemainingSpace());
     }
-     
-    // Inner class representing a file
+
     public static class File {
         private String name;
-        private int size; // in MB
-        
+        private int size;
+
         public File(String name, int size) {
             this.name = name;
             this.size = size;
         }
-        
+
         public String getName() {
             return name;
         }
-        
+
         public int getSize() {
             return size;
         }
